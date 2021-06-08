57162539adaa26e7970151f630141267d8793364
AaronCarrete <asantacruzcarrete@gmail.com>
2021-06-08 00:00:14 -0500

    Se eliminó código innecesario de CSS

--- code.js
+++ code.js
@@ -1,35 +1,18 @@
-window.addEventListener('DOMContentLoaded', () => {
-	const container = document.querySelector('.hour');
-	const btn = document.querySelector('.constop');
-	// let id = 0;
+const container = document.querySelector('.hour');
+const btn = document.querySelector('.constop');
+let id = 0;
 
-	const updateHour = setInterval(() => {
-		const date = new Date();
-		container.innerHTML = `${date.getHours()} : ${date.getMinutes()} : ${date.getSeconds()}`;
-		let id = 1;
-	}, 1000);
+const updateHour = setInterval(() => {
+	const date = new Date();
+	container.innerHTML = `${date.getHours()} : ${date.getMinutes()} : ${date.getSeconds()}`;
+}, 1000);
 
 
-	btn.addEventListener('click', e => {
-
-		if (btn.innerText === 'Continue') {
-			const updateHour = setInterval(() => {
-			const date = new Date();
-			container.innerHTML = `${date.getHours()} : ${date.getMinutes()} : ${date.getSeconds()}`;
-			}, 1000);
-			let id = 1;
-			btn.innerText = 'Stop';
-		} else if (btn.innerText === 'Stop') {
-			for (i=0; i<100; i++) {
-    			window.clearInterval(i);
-    			btn.innerText = 'Continue';
-			}
-
-			
-		}
-
-		e.preventDefault();
-	});
+btn.onclick = e => {
+	clearInterval(1);
+	btn.innerText = 'Continue';
+	id++; 
+	e.preventDefault();
+};
 
 
-})
